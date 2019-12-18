3e5279c7b1ff2dd5e867d1a83d28d971b697d7cc
Trevor315 <trevork666@gmail.com>
2019-12-18 12:30:32 -0500

    Making small progress

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 var question = "What is FairChild Semiconductor?"
 var answers = ['Red', 'Green', 'Blue', 'Yellow'];
+var answerButtons = document.getElementsByClassName("quizButton");
+var correct = 3;
+
+for(var spot=0; spot<answers.length; spot++)
+{
+    answerButtons[spot].innerHTML = answers[spot];
+
+    answerButtons[spot].addEventListener('click', function() {
+        disableAll();
+        if(this.innerHTML == answers[correct]) {
+            this.setAttribute('style','background-color:rgb(76,175,80);');
+        }else {
+            this.setAttribute('style','background-color:rgb(215,50,35);');    
+        }
+    });
+}
+
+
+
+document.getElementById('question').innerHTML = question;
+
+function changeColor() {
+    document.getElementById('Question1').setAttribute('style','background-color:rgb(210,50,35);')
+}
+
+function disableAll() {
+    for(var num=0; num<answerButtons.length; num++) {
+        answerButtons[num].setAttribute('disabled',true);
+    }
+}
